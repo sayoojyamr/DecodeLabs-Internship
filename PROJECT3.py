b31397946f636c9a0aa99deab9a3765f01123b54
sayoojyamr <mrsayoojya@gmail.com>
2026-04-05 20:43:23 +0530

    Added PROJECT3

---
+++ PROJECT3.py
@@ -0,0 +1,30 @@
+## Project title - Random Password Generator (Python)
+
+## Description
+This is a Python program that generates a random password based on the length provided by the user.
+
+The password includes a mix of letters and numbers to make it more secure.
+
+This project demonstrates:
+- Importing modules (`random`)
+- String manipulation
+- User input
+- Loops
+
+## How to Run
+
+1. Make sure Python is installed.
+
+2. Open terminal or command prompt.
+
+3. Navigate to the project folder:
+   cd <DECODELABS>
+
+4. Run the program:
+   python main.py
+
+## Example Usage
+
+Enter password length: 8  
+
+Generated Password: A7kP2xQ9
